1e23a5a84e0335314f0d7dcc0322974d209b3232
cristicadis <cristi.cadis@yahoo.com>
2025-01-11 13:35:11 +0200

    warnings

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
   { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tour", text: "tour" },
+  { id: 4, href: "#tours", text: "tour" },
 ];
 
 export const socialLinks = [
